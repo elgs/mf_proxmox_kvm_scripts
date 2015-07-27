8c2381a2f43ce4d7666e81c01a749fae2f306d75
elgs <elgs1980@hotmail.com>
2015-07-27 20:17:10 +0800

    Update.

--- openvz/ppp_on.sh
+++ openvz/ppp_on.sh
@@ -5,3 +5,8 @@ IN=$@
 IFS=',' read varvmid varhostname varusername varpassword varmac varip varnode <<< "$IN";IFS='=' read var1 vmid <<< "$varvmid";IFS='=' read var2 hostname <<< "$varhostname";IFS='=' read var3 password <<< "$varpassword";IFS='=' read var4 username <<< "$varusername";IFS='=' read var5 macs <<< "$varmac";IFS='=' read var6 ips <<< "$varip";IFS='=' read var7 node <<< "$varnode"
 
 vzctl set ${vmid} --features ppp:on --save
+vzctl start ${vmid}
+vzctl set ${vmid} --devices c:108:0:rw --save
+vzctl exec ${vmid} mknod /dev/ppp c 108 0
+vzctl exec ${vmid} chmod 600 /dev/ppp
+vzctl stop ${vmid}
