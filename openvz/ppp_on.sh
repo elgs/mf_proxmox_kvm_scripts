282a346bf272239c4ba26ec913f7f15ee9441583
Elgs Qian Chen <elgs1980@gmail.com>
2015-08-15 07:14:12 -0400

    Update.

--- openvz/ppp_on.sh
+++ openvz/ppp_on.sh
@@ -4,6 +4,7 @@ IN=$@
 
 IFS=',' read varvmid varhostname varusername varpassword varmac varip varnode <<< "$IN";IFS='=' read var1 vmid <<< "$varvmid";IFS='=' read var2 hostname <<< "$varhostname";IFS='=' read var3 password <<< "$varpassword";IFS='=' read var4 username <<< "$varusername";IFS='=' read var5 macs <<< "$varmac";IFS='=' read var6 ips <<< "$varip";IFS='=' read var7 node <<< "$varnode"
 
+
 vzctl set ${vmid} --features ppp:on --save
 vzctl set ${vmid} --devnodes net/tun:rw --capability net_admin:on --save
 vzctl start ${vmid}
